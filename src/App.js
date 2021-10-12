c2406f45592ba23ed75abc5315a3136d9f54b991
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2021-10-11 23:55:42 -0400

    First react Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,173 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Image,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Row,
+} from "react-bootstrap";
+import { images } from "./IMAGES";
+
+import loveIcon from "./assets/icons/loveIcon.svg";
+import trashIcon from "./assets/icons/trashIcon.svg";
+
+const videos = [
+  { title: "Tyrion", link: "https://www.youtube.com/watch?v=H9HFigfkKHA" },
+  { title: "Euron", link: "https://www.youtube.com/watch?v=sbX_ak0N1EI" },
+  { title: "Cersei", link: "https://www.youtube.com/watch?v=K5TX2aLdB4s" },
+  { title: "Summerhall", link: "https://www.youtube.com/watch?v=85P2-4NZqLM" },
+];
+
+function Dropdown(props) {
+  const videos = props.videos;
+  const videoItems = videos.map((video) => (
+    <NavDropdown.Item key={video.link} href={video.link} target="blank">
+      {video.title}
+    </NavDropdown.Item>
+  ));
+  return videoItems;
+}
+
+class NavigationBar extends React.Component {
+  happyURL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  sadURL = "https://www.youtube.com/watch?v=hiwgOWo7mDc";
+
+  render() {
+    return (
+      <Navbar bg="lght" expand="lg" style={{ borderStyle: "solid" }}>
+        <Container>
+          <Navbar.Brand href="#home">Axolotl</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link href={this.happyURL} target="blank">
+                {" "}
+                Happy Song{" "}
+              </Nav.Link>
+              <Nav.Link href={this.sadURL} target="blank">
+                {" "}
+                Fun Song{" "}
+              </Nav.Link>
+              <NavDropdown title="Playlist" id="basic-nav-dropdown">
+                <Dropdown videos={videos} />
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+class CardFooterContent extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      likes: 0,
+      trash: 0,
+    };
+  }
+  handleLike = this.handleLike.bind(this);
+
+  handleLike() {
+    this.setState((prevState) => ({
+      likes: prevState.likes + 1,
+      trash: prevState.trash,
+    }));
+  }
+
+  handleTrash = () => {
+      this.setState((prevState) => ({
+          likes: prevState.likes,
+          trash: prevState.trash + 1,
+      }));
+  }
+
+  render() {
+    return (
+      <Row>
+        <Col>
+          <Button variant="light" onClick={this.handleLike}>
+            <Image src={loveIcon} thumbnail />
+          </Button>
+        </Col>
+        <Col xs={2}>
+          <h1>{this.state.likes}</h1>
+        </Col>
+        <Col xs={2}>
+          <h1>{this.state.trash}</h1>
+        </Col>
+        <Col>
+          <Button variant="light" onClick={this.handleTrash}>
+            <Image src={trashIcon} thumbnail />
+          </Button>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+function CardContent(props) {
+  const cardInfo = props.card;
+
+  return (
+    <Container fluid>
+      <Col>
+        <Card style={{ width: "250px" }}>
+          <Card.Img
+            style={{ height: "200px", width: "200px" }}
+            variant="top"
+            src={cardInfo.src}
+          />
+          <Card.Body>
+            <Card.Title>{cardInfo.description}</Card.Title>
+          </Card.Body>
+          <CardFooterContent></CardFooterContent>
+        </Card>
+      </Col>
+    </Container>
+  );
+}
+
+class TableContent extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      tableColumns: 4,
+      cards: images,
+      rowNumber: 0,
+    };
+  }
+
+  render() {
+    return (
+      <Row xs={2} md={4}>
+        {this.state.cards.map((value, index) => {
+          return <CardContent key={value.id} card={value} />;
+        })}
+      </Row>
+    );
+  }
+}
+
+class BodyContent extends React.Component {
+  render() {
+    return <TableContent />;
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavigationBar></NavigationBar>
+      <BodyContent></BodyContent>
+    </>
   );
 }
 
---
+++ src/IMAGES.js
@@ -0,0 +1,135 @@
+import dragon1 from './assets/dragons/dragon1.png';
+import dragon2 from './assets/dragons/dragon2.png';
+import dragon3 from './assets/dragons/dragon3.png';
+import dragon4 from './assets/dragons/dragon4.png';
+import dragon5 from './assets/dragons/dragon5.png';
+import dragon6 from './assets/dragons/dragon6.png';
+import dragon7 from './assets/dragons/dragon7.png';
+import dragon8 from './assets/dragons/dragon8.png';
+
+import axolotl1 from './assets/axolotl/axolotl1.png';
+import axolotl2 from './assets/axolotl/axolotl2.png';
+import axolotl3 from './assets/axolotl/axolotl3.png';
+import axolotl4 from './assets/axolotl/axolotl4.png';
+import axolotl5 from './assets/axolotl/axolotl5.png';
+import axolotl6 from './assets/axolotl/axolotl6.png';
+import axolotl7 from './assets/axolotl/axolotl7.png';
+import axolotl8 from './assets/axolotl/axolotl8.png';
+import axolotl9 from './assets/axolotl/axolotl9.png';
+import axolotl10 from './assets/axolotl/axolotl10.png';
+
+
+export const dragonImages = [
+  {
+    id: 1,
+    src: dragon1,
+    title: "Dragon 1",
+    description: "Fire",
+  },
+  {
+    id: 2,
+    src: dragon2,
+    title: "Dragon 2",
+    description: "Earth",
+  },
+  {
+    id: 3,
+    src: dragon3,
+    title: "Dragon 3",
+    description: "Water",
+  },
+  {
+    id: 4,
+    src: dragon4,
+    title: "Dragon 4",
+    description: "Fire",
+  },
+  {
+    id: 5,
+    src: dragon5,
+    title: "Dragon 5",
+    description: "Pain",
+  },
+  {
+    id: 6,
+    src: dragon6,
+    title: "Dragon 6",
+    description: "Kindness",
+  },
+  {
+    id: 7,
+    src: dragon7,
+    title: "Dragon 7",
+    description: "Existence",
+  },
+  {
+    id: 8,
+    src: dragon8,
+    title: "Dragon 8",
+    description: "Hope",
+  },
+];
+
+export const images = [
+  {
+    id: 1,
+    src: axolotl1,
+    title: "Axolotl 1",
+    description: "Angry",
+  },
+  {
+    id: 2,
+    src: axolotl2,
+    title: "Axolotl 2",
+    description: "Boring",
+  },
+  {
+    id: 3,
+    src: axolotl3,
+    title: "Axolotl 3",
+    description: "Oblivious",
+  },
+  {
+    id: 4,
+    src: axolotl4,
+    title: "Axolotl 4",
+    description: "Eating",
+  },
+  {
+    id: 5,
+    src: axolotl5,
+    title: "Axolotl 5",
+    description: "Food Coma",
+  },
+  {
+    id: 6,
+    src: axolotl6,
+    title: "Axolotl 6",
+    description: "Happy",
+  },
+  {
+    id: 7,
+    src: axolotl7,
+    title: "Axolotl 7",
+    description: "Greeting",
+  },
+  {
+    id: 8,
+    src: axolotl8,
+    title: "Axolotl 8",
+    description: "Pillow",
+  },
+  {
+    id: 9,
+    src: axolotl9,
+    title: "Axolotl 9",
+    description: "Laughing",
+  },
+  {
+    id: 10,
+    src: axolotl10,
+    title: "Axolotl 10",
+    description: "Love",
+  },
+
+]
